9ccd62fbe051d0d561e462e2aeb4e14ad18f3022
airton-mendoza <airton.mendoza@usil.pe>
2021-05-18 22:02:21 -0500

    index

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnMenu.addEventListener('click', function(){
+  menu.classList.toggle('mostrar');
+});
